bdb2b3db472b697d409d8dd111bdfff356e75864
Ramon Torres <raymondjavaxx@gmail.com>
2023-11-14 22:21:59 -0500

    Cleanup

--- src/rtris/sound.rb
+++ src/rtris/sound.rb
@@ -1,25 +1,5 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2010 Ramon E. Torres
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 module Rtris
   class Sound
     def initialize
@@ -31,10 +11,6 @@ module Rtris
       @beam_sfx = Gosu::Sample.new("#{sfx_dir}/beam.wav")
 
       @line_voices = []
-      # @line_voices.push(Gosu::Sample.new(sfx_dir + "/voices/double.wav"))
-      # @line_voices.push(Gosu::Sample.new(sfx_dir + "/voices/double.wav"))
-      # @line_voices.push(Gosu::Sample.new(sfx_dir + "/voices/triple.wav"))
-      # @line_voices.push(Gosu::Sample.new(sfx_dir + "/voices/tetris.wav"))
     end
 
     def play_music
